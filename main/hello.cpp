ef9f727f1788aa1def758854267c9d77b9da14d6
Gagan H R <hrgagan4@gmail.com>
2024-03-08 20:24:52 +0530

    Describe this commit

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Hello, I am PES1UG21CS196\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
